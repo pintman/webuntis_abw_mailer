75df04c83d4065e84f4ce479d6a3170cc49c2e4c
Marco Bakera <marco@bakera.de>
2021-09-12 13:50:55 +0200

    sample template

---
+++ config_template.py
@@ -0,0 +1,13 @@
+# copy this file to config.py and fill out
+
+AUSBILDER = {
+    'ITF19a': {
+        'Schmitz': 'meier@example.org',
+        'Meiler': 'mueller@example.org',
+    }
+}
+
+SUBJECT_TEMPLATE = "Versäumter Unttericht {langname}"
+
+BODY_TEMPLATE = """{langname} hat gefehlt am {beginndatum} {beginnzeit} bis {enddatum} {endzeit}
+"""
